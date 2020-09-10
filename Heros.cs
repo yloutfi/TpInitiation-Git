e9c6e614aa6caf6f80c11c564891ca949dd753c5
yloutfi <yloutfi@sio.jjr>
2020-09-10 16:54:36 +0200

    ajout méthode frapper à la classe Personnage

--- Heros.cs
+++ Heros.cs
@@ -1,3 +1,3 @@
-class personnage:Heros{
+class Heros:Personnage{
     
 }
--- Personnage.cs
+++ Personnage.cs
@@ -1,3 +1,6 @@
-class Heros:Personnage{
+class Personnage{
+    void frapper(Personnage personage, int force){
+        
+    }
     
 }
